f7262d147e4915f82407e494b204b0ddf00e855c
Madlson <120780771+Madlson@users.noreply.github.com>
2023-02-12 03:43:56 +0600

    210103449 Madi Meirambay

---
+++ problem-B/ProblemB2/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape sh1 = new Shape();
+        Shape sh2 = new Shape("black", false);
+
+        System.out.println(sh1);
+        System.out.println(sh2);
+
+        Circle c1 = new Circle();
+        Circle c2 = new Circle(2.1256);
+
+        System.out.println(c1);
+        System.out.println(c2);
+
+
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = new Rectangle(3.0 , 4.0);
+
+        System.out.println(r1);
+        System.out.println(r2);
+
+        Circle circle = new Circle();
+        System.out.println(circle.getColor());
+        System.out.println(circle.isFilled());
+        System.out.println(circle.getRadius());
+
+        Rectangle rectangle = new Rectangle(3.8, 2.5, "green", true);
+        System.out.println(rectangle);
+        System.out.println(rectangle.getArea());
+        System.out.println(rectangle.getPerimeter());
+        System.out.println(rectangle.getColor());
+        System.out.println(rectangle.getLength());
+
+        Square square = new Square(6.6);
+        System.out.println(square);
+        System.out.println(square.getArea());
+        System.out.println(square.getColor());
+        System.out.println(square.getSide());
+    }
+}
